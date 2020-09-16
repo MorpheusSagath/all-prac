db9cd01c78629f3cb8e1ec05845889a8f10bc17b
Polina Kovalenko <parosonisi@gmail.com>
2020-09-16 14:14:15 +0300

    book.

---
+++ bookbook/src/Osnovnaya.java
@@ -0,0 +1,8 @@
+public class Osnovnaya {
+    public static void main(String[] args) {
+        Book a = new Book();
+        a.author = "K. Golden";
+        a.name = "World of Warcraft: Arthas. Rise of the Lich King";
+        a.numPages = 414;
+    }
+}
