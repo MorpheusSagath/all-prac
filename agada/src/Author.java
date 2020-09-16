53734589207545f061bf64dc188d27b1f65bf301
Polina Kovalenko <parosonisi@gmail.com>
2020-09-16 20:35:49 +0300

    author

---
+++ agada/src/Author.java
@@ -0,0 +1,34 @@
+public class Author {
+    public String name;
+    public String email;
+    public char gender;
+
+    public Author(String name, String email, char gender)
+    {
+        this.email = email;
+        this.gender = gender;
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+    public String toString()
+    {
+        return  "Name: " + name + "\n" + "Genger: " + gender + "\n" + "Email: " + email;
+    }
+}
